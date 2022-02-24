b1a3695e99ede16cb30929aef732616c7f0499d8
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 10:01:41 -0500

    setting up app

---
+++ src/main/java/com/project/mygrocerylistbackend/MyGroceryListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.mygrocerylistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGroceryListBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGroceryListBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/mygrocerylistbackend/MyGroceryListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mygrocerylistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGroceryListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
