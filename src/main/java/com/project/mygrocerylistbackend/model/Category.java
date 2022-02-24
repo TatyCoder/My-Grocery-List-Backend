d1d5e2d2f5cbfc813937e25a2b48c4c03d23a8c6
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 10:51:33 -0500

    Category model created

---
+++ src/main/java/com/project/mygrocerylistbackend/model/Category.java
@@ -0,0 +1,47 @@
+package com.project.mygrocerylistbackend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    public Category(Long categoryId, String name) {
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
