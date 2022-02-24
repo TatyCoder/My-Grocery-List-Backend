3db496b7ec7a609398f4f3c646ce2e0da7664663
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 16:08:52 -0500

    getAllUsers implemented

--- src/main/java/com/project/mygrocerylistbackend/service/UserService.java
+++ src/main/java/com/project/mygrocerylistbackend/service/UserService.java
@@ -1,8 +1,11 @@
 package com.project.mygrocerylistbackend.service;
 
+import com.project.mygrocerylistbackend.model.User;
 import com.project.mygrocerylistbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -12,4 +15,10 @@ public class UserService {
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    public List<User> getAllUsers() {
+        List<User> user = this.userRepository.findAll();
+        return user;
+    }
+
 }
