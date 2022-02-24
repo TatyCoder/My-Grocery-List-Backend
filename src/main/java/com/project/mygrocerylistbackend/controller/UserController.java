70bd48e39639aa198a5ea6f805dfedbc0de2b16d
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 14:35:22 -0500

    getAllUsers created

--- src/main/java/com/project/mygrocerylistbackend/controller/UserController.java
+++ src/main/java/com/project/mygrocerylistbackend/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.project.mygrocerylistbackend.controller;
 
+import com.project.mygrocerylistbackend.model.User;
 import com.project.mygrocerylistbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/api")    // url -> http://localhost:9092/api
+@RequestMapping(path = "/api")   // url -> http://localhost:9092/api
 public class UserController {
 
     private UserService userService;
@@ -16,4 +20,9 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/user")   // url -> http://localhost:9092/api/user
+    public List<User> getAllUsers() {
+        System.out.println("calling getAllUsers");
+        return userService.getAllUsers();
+    }
 }
