8e0e1af44c69f44da2401d389f70534144064bf1
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 10:21:29 -0500

    user model created

---
+++ src/main/java/com/project/mygrocerylistbackend/model/User.java
@@ -0,0 +1,51 @@
+package com.project.mygrocerylistbackend.model;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    public User(Long userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
