3ffd97387b5148805d70b35dc4ef981f329b9baa
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 16:28:59 -0500

    createUser implemented

--- src/main/java/com/project/mygrocerylistbackend/repository/UserRepository.java
+++ src/main/java/com/project/mygrocerylistbackend/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.project.mygrocerylistbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String userName);
 }
--- src/main/java/com/project/mygrocerylistbackend/service/UserService.java
+++ src/main/java/com/project/mygrocerylistbackend/service/UserService.java
@@ -1,11 +1,13 @@
 package com.project.mygrocerylistbackend.service;
 
+import com.project.mygrocerylistbackend.exceptions.InformationExistException;
 import com.project.mygrocerylistbackend.model.User;
 import com.project.mygrocerylistbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,6 +21,15 @@ public class UserService {
     public List<User> getAllUsers() {
         List<User> user = this.userRepository.findAll();
         return user;
+    }
+
+    public User createUser(User userObject) {
+        User user = userRepository.findByName(userObject.getName());
+        if (user != null) {
+            throw new InformationExistException("user with name " + user.getName() + " already exists.");
+        } else {
+            return userRepository.save(userObject);
+        }
     }
 
 }
