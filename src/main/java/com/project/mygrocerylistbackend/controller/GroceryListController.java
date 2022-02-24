a7253a7e3ee9b737df00005bdcf72fb92da07371
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 17:15:58 -0500

    createGroceryList created

--- src/main/java/com/project/mygrocerylistbackend/controller/GroceryListController.java
+++ src/main/java/com/project/mygrocerylistbackend/controller/GroceryListController.java
@@ -4,10 +4,7 @@ import com.project.mygrocerylistbackend.model.GroceryList;
 import com.project.mygrocerylistbackend.model.User;
 import com.project.mygrocerylistbackend.service.GroceryListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +23,12 @@ public class GroceryListController {
     public List<GroceryList> getAllGroceryLists(@PathVariable Long userId) {
         System.out.println("calling getAllGroceryLists");
         return groceryListService.getAllGroceryLists(userId);
+    }
+
+    @PostMapping("/user/{userId}/groceryList")   // url -> http://localhost:9092/api/user/userId/groceryList
+    public GroceryList createGroceryList(@RequestBody GroceryList groceryListObject) {
+        System.out.println("calling createGroceryList");
+        return groceryListService.createGroceryList(groceryListObject);
     }
 
 }
