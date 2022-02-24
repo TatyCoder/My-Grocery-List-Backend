16dac7996686dd5e27e65d3c8d0ad89461d6e226
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 14:15:50 -0500

    UserRepository created

---
+++ src/main/java/com/project/mygrocerylistbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.project.mygrocerylistbackend.repository;
+
+import com.project.mygrocerylistbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
