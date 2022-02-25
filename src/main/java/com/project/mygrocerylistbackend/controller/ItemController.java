f998823c7d24358bb3896d557efc69ed62c321d2
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-24 20:29:56 -0500

    createItem created

--- src/main/java/com/project/mygrocerylistbackend/controller/ItemController.java
+++ src/main/java/com/project/mygrocerylistbackend/controller/ItemController.java
@@ -1,9 +1,9 @@
 package com.project.mygrocerylistbackend.controller;
 
+import com.project.mygrocerylistbackend.model.Item;
 import com.project.mygrocerylistbackend.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api")    // url -> http://localhost:9092/api
@@ -15,4 +15,11 @@ public class ItemController {
     public void setItemService(ItemService itemService) {
         this.itemService = itemService;
     }
+
+    @PostMapping("/user/{userId}/groceryList/{groceryListId}/item")   // url -> http://localhost:9092/api/user/userId/groceryList/groceryListId
+    public Item createItem(@PathVariable Long groceryListId, @RequestBody Item itemObject) {
+        System.out.println("calling createItem");
+        return itemService.createItem(groceryListId, itemObject);
+    }
+
 }
