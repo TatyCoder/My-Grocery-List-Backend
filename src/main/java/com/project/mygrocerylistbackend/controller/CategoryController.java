59d68db54d0ccf650797c7a893a487b30ef7a3c8
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-25 14:45:14 -0500

    getCategory created

--- src/main/java/com/project/mygrocerylistbackend/controller/CategoryController.java
+++ src/main/java/com/project/mygrocerylistbackend/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.project.mygrocerylistbackend.controller;
 
+import com.project.mygrocerylistbackend.model.Category;
 import com.project.mygrocerylistbackend.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,11 @@ public class CategoryController {
     @Autowired
     public void setCategoryService(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping("/user/{userId}/groceryList/{groceryListId}/item/{itemId}/category/{categoryId}")   // url -> http://localhost:9092/api/user/userId/groceryList/groceryListId/item/itemId/category/categoryId
+    public Category getCategory(@PathVariable Long userId, @PathVariable Long groceryListId, @PathVariable Long itemId, @PathVariable Long categoryId) {
+        System.out.println("calling getCategory");
+        return categoryService.getCategory(userId, groceryListId, itemId, categoryId);
     }
 }
