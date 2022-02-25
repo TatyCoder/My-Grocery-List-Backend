1d2cf7cef3986d37e574235acc13ad72bb3eeafa
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-25 12:07:03 -0500

    CategoryService created

---
+++ src/main/java/com/project/mygrocerylistbackend/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.project.mygrocerylistbackend.service;
+
+import com.project.mygrocerylistbackend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public void setCategoryRepository(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+}
