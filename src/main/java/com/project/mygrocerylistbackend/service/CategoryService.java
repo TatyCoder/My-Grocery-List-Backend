8da58e6a409df7ecb1517248d36c2ce443cb9ab2
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-25 15:20:12 -0500

    getCategory created

--- src/main/java/com/project/mygrocerylistbackend/service/CategoryService.java
+++ src/main/java/com/project/mygrocerylistbackend/service/CategoryService.java
@@ -1,5 +1,8 @@
 package com.project.mygrocerylistbackend.service;
 
+import com.project.mygrocerylistbackend.exceptions.InformationNotFoundException;
+import com.project.mygrocerylistbackend.model.Category;
+import com.project.mygrocerylistbackend.model.Item;
 import com.project.mygrocerylistbackend.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +14,23 @@ public class CategoryService {
     @Autowired
     public void setCategoryRepository(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    private ItemService itemService;
+
+    @Autowired
+    public void setItemService(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    public Category getCategory(Long userId, Long groceryListId, Long itemId, Long categoryId) {
+        // This is calling getItem() from ItemService.
+        Item item = itemService.getItem(userId, groceryListId, itemId);
+        Category category = categoryRepository.getCategoryByCategoryId(categoryId);
+        if (category != null) {
+            return category;
+        } else {
+            throw new InformationNotFoundException("category with id " + categoryId + " not found.");
+        }
     }
 }
