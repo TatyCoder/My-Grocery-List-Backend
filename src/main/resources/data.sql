5a9f76730d949425c81c09c6958e97cd42e2727e
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-25 21:12:07 -0500

    Prepopulate category table with all categories

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO category (name) VALUES ('Produce');
+-- INSERT INTO category (name) VALUES ('Beverages');
+-- INSERT INTO category (name) VALUES ('Bread & Bakery');
+-- INSERT INTO category (name) VALUES ('Condiments, Spices & Baking');
+-- INSERT INTO category (name) VALUES ('Dairy, Eggs & Cheese');
+-- INSERT INTO category (name) VALUES ('Grains, Pasta & Cereal');
+-- INSERT INTO category (name) VALUES ('Frozen Foods');
+-- INSERT INTO category (name) VALUES ('Meat & Seafood');
+-- INSERT INTO category (name) VALUES ('Sauces & Oils');
+-- INSERT INTO category (name) VALUES ('Snacks & Candy');
+-- INSERT INTO category (name) VALUES ('Personal & Health Care');
+-- INSERT INTO category (name) VALUES ('Household & Cleaning Supplies');
+-- INSERT INTO category (name) VALUES ('Other');
